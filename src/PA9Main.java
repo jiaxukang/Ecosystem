ddbe952ac308befdb5c99574ad74d3d07813d1e5
Michelle Strout <mstrout@cs.arizona.edu>
2018-04-08 08:53:41 -0700

    Simple example of processing the text from a text field.

---
+++ src/PA9Main.java
@@ -0,0 +1,40 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+
+public class PA9Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        // ===== set up the scene with a text box and button for input
+        BorderPane p = new BorderPane();
+        TextField cmd_in = new TextField();
+        Button button = new Button("Process");
+        final int num_items = 2;
+        HBox input_box = new HBox(num_items);
+        input_box.getChildren().add(cmd_in);
+        input_box.getChildren().add(button);
+        p.setBottom(input_box);
+
+        // === Example of how to set up processing input from a text area
+        button.setOnAction((event) -> {
+            System.out.println(cmd_in.getText());
+        });
+
+        // Connect the border pane into the scene and show the window.
+        primaryStage.setTitle("Sample input field");
+        primaryStage.setScene(new Scene(p));
+        primaryStage.show();
+    }
+
+}
